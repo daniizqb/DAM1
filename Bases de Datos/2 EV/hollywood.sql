40b3b6d1876420fe05dd7e391199280742b09602
daniizqb <danizquiboni@gmail.com>
2021-12-14 14:19:33 +0100

    14/12/21 14:19

--- Bases de Datos/2 EV/hollywood.sql
+++ Bases de Datos/2 EV/hollywood.sql
@@ -48,3 +48,22 @@ ALTER TABLE PELICULAS ADD FOREIGN KEY (nom_dir) REFERENCES DIRECTORES(nombre);
 ALTER TABLE PELICULAS ADD FOREIGN KEY (nom_prot) REFERENCES ACTORES(nombre);
 ALTER TABLE ACTUAN ADD FOREIGN KEY (nom_peli) REFERENCES PELICULAS(nombre);
 ALTER TABLE ACTUAN ADD FOREIGN KEY (nom_actor) REFERENCES ACTORES(nombre);
+
+-- Insercion de datos
+
+INSERT INTO ACTORES VALUES
+('Harrison Ford','M','1942-07-13','USA'),
+('Tom Cruise','M','1962-07-03','USA'),
+('Tony Curtis','M','1925-06-03-','USA'),
+('Penelope Cruz','F','1974-04-28','Spain'),
+('Leonardo DiCaprio','M','1974-11-11-','USA'),
+('Ursula Corbero','F','1989-08-11','Spain'),
+('Benicio del Toro','M','1967-02-19','Puerto Rico'),
+('Tom Holland','M','1996-06-01','USA'),
+('Angelina Jolie','F','1975-06-04','Spain');
+
+INSERT INTO DIRECTORES VALUES
+('Harrison Ford','M','1942-07-13','USA'),
+('Clint Eastwood','M','1962-07-03','USA'),
+('Stanley Kubric','M','1925-06-03-','USA'),
+('Quentin Tarantino','F','1974-04-28','USA'),
