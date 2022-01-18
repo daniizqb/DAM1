0f70f718577492c3d17e39df91ce13bd1008ddd1
daniizqb <danizquiboni@gmail.com>
2022-01-18 23:42:28 +0100

    a

---
+++ Programacion/C/Izquierdo Daniel - UEFA/cmake-build-debug/CMakeFiles/Izquierdo_Daniel___UEFA.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Izquierdo_Daniel___UEFA.dir/main.c.o"
+  "Izquierdo_Daniel___UEFA"
+  "Izquierdo_Daniel___UEFA.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/Izquierdo_Daniel___UEFA.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
