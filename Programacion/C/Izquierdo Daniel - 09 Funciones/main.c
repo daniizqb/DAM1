de572da1918996513c60b0c601567d80b52b96a2
daniizqb <danizquiboni@gmail.com>
2022-01-18 22:50:05 +0100

    18/01/22 22:50

--- Programacion/C/Izquierdo Daniel - 09 Funciones/main.c
+++ Programacion/C/Izquierdo Daniel - 09 Funciones/main.c
@@ -1,31 +1,49 @@
 #include <stdio.h>
-#include <string.h>
-#define TAM 11
-
+#define TAMS 11
+#define TAMA 10
 int strLenght (const char string[]);
+void strBool (short aNum[],short num, short *bool);
 int main() {
-    int i=0;
-    char cadena [TAM]="";
-
-    for (i = 0;i < TAM;i++) {
-        cadena[i] = ' ';
-    }
+    int i = 0;
+    short aNum [TAMA] = {2,5,7,5,2,8,4,0,9,1}, userNum = 0, bool = 0;
+    char cadena [TAMS] = "";
 
     printf("Introduce Cadena: ");
     scanf("%s",cadena);
     fflush(stdin);
 
-    printf("La cadena contiene %d caracteres",strLenght (cadena));
+    printf("La cadena contiene %d caracteres\n\n",strLenght(cadena));
+
+    printf("Introduce un numero del 0 al 9\n");
+    do {
+        scanf("%hd",&userNum);
+        fflush(stdin);
+    } while (userNum < 0 || userNum > 9);
+
+    strBool(aNum,userNum,&bool);
+    printf("%hd %s",bool, bool?"uno":"cero");
 
     return 0;
 }
 
 int strLenght (const char string[]) {
     int i = 0, cont = 0;
-    for (i = 0;i < TAM-1;i++) {
-        if (string[i] != ' ')
-            cont++;
-    }
+    do {
+        i++;
+        cont++;
+    } while (string[i] != '\0' && i <= TAMS);
 
     return cont;
+}
+
+void strBool (short aNum[],short num, short *bool) {
+    int i = 0;
+
+    do {
+        if (aNum[i] == num)
+            *bool = 1;
+        i++;
+    } while (i < TAMA && bool == 0);
+
+    return;
 }
