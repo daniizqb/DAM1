838408016aa8e3d730d6622832d28f74d7a0180f
daniizqb <danizquiboni@gmail.com>
2022-01-19 17:50:06 +0100

    19/01/22 17:50

--- Programacion/C/Ej20/main.c
+++ Programacion/C/Ej20/main.c
@@ -22,91 +22,64 @@
 
 #define TAMCAD 51
 
-char esPalindromo (char cad);
+short esPalindromo (char cad);
+int myCtype (int caso, char cad[]);
 
 int main() {
-    int i = 0, cont = 0;
+    int i = 0, cont = 0, caso = 0;
     char cad [TAMCAD] = "Hola Buenos d1as :)";
 
     /*puts("Primera cadena: ");
     gets(cad);*/
 
+    caso++;
     puts("Digitos: ");
-    cont = 0;
-    i = 0;
-    do {
-        if (isdigit(cad[i]))
-            cont++;
-        i++;
-    } while (i <= TAMCAD && cad[i] != '\0');
-    printf("%d\n",cont);
+    printf("%d\n", myCtype(caso,cad));
 
+    caso++;
     puts("Minusculas: ");
-    cont = 0;
-    i = 0;
-    do {
-        if (islower(cad[i]))
-            cont++;
-        i++;
-    } while (i <= TAMCAD && cad[i] != '\0');
-    printf("%d\n",cont);
+    printf("%d\n", myCtype(caso,cad));
 
+    caso++;
     puts("Mayusculas: ");
-    cont = 0;
-    i = 0;
-    do {
-        if (isupper(cad[i]))
-            cont++;
-        i++;
-    } while (i <= TAMCAD && cad[i] != '\0');
-    printf("%d\n",cont);
+    printf("%d\n", myCtype(caso,cad));
 
+    caso++;
     puts("Espacios: ");
-    cont = 0;
-    i = 0;
-    do {
-        if (isspace(cad[i]))
-            cont++;
-        i++;
-    } while (i <= TAMCAD && cad[i] != '\0');
-    printf("%d\n",cont);
+    printf("%d\n", myCtype(caso,cad));
 
+    caso++;
     puts("Simbolos: ");
-    cont = 0;
-    i = 0;
-    do {
-        if (ispunct(cad[i]))
-            cont++;
-        i++;
-    } while (i <= TAMCAD && cad[i] != '\0');
-    printf("%d\n",cont);
+    printf("%d\n", myCtype(caso,cad));
 
+    caso++;
     puts("Alfanumericos: ");
-    cont = 0;
-    i = 0;
-    do {
-        if (isalnum(cad[i]))
-            cont++;
-        i++;
-    } while (i <= TAMCAD && cad[i] != '\0');
-    printf("%d\n",cont);
+    printf("%d\n", myCtype(caso,cad));
 
     //printf("%s un palindromo", esPalindromo(cad)?"Es":"No es");
 
     return 0;
 }
 
-/*int myCtype (int funcion, char cad) {
+int myCtype (int caso, char cad[]) {
     int i = 0, cont = 0;
     do {
-        if (funcion)
-            cont++;
+        switch (caso) {
+            case 1: if (isdigit(cad[i])){cont++;}break;
+            case 2: if (islower(cad[i])){cont++;}break;
+            case 3: if (isupper(cad[i])){cont++;}break;
+            case 4: if (isspace(cad[i])){cont++;}break;
+            case 5: if (ispunct(cad[i])){cont++;}break;
+            case 6: if (isalnum(cad[i])){cont++;}break;
+            default: printf("Caracter no valido");break;
+        }
         i++;
     } while (i <= TAMCAD && cad[i] != '\0');
-    printf("%d\n",cont);
-}*/
 
-char esPalindromo (char cad) {
+    return cont;
+}
+
+short esPalindromo (char cad) {
 
     if (stricmp(cad, strrev(cad)))
         return 1;
