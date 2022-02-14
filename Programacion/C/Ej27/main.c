edc80d16be34078072ccc80903e2c0a9dcea3efd
daniizqb <danizquiboni@gmail.com>
2022-02-14 14:18:53 +0100

    14/02/22 14:18

--- Programacion/C/Ej27/main.c
+++ Programacion/C/Ej27/main.c
@@ -11,38 +11,56 @@ int main() {
     FILE *fich;
     fich = fopen("text.txt","wb+");
 
-    short userNum = 0, cont = 0;
+    short userNum = 0, cont = 0, regCont = 0, sumatorio = 0;
     struct data dato = {"",0};
 
 
     strcpy(dato.nombre,"Suma:");
     dato.num = 0;
     fwrite(&dato,sizeof(dato),1,fich);
+    regCont++;
 
     strcpy(dato.nombre,"Numero:");
-    while (userNum != -1) {
-        printf("Numero:");
-        scanf("%hd",&userNum);
-        fflush(stdin);
-        dato.num = userNum;
-        fwrite(&dato,sizeof(dato),1,fich);
-    }
 
-    /*do {
+    do {
         printf("Numero: ");
         scanf("%hd",&userNum);
         fflush(stdin);
         if (userNum == -1) {
             cont++;
-            if (cont == 1)
-                strcpy(dato.nombre,"Resta:");
-            else
-                strcpy(dato.nombre,"Multiplicacion:");
-        } else if (userNum >= 0) {
+            if (cont == 1) {
+                strcpy(dato.nombre, "Resta:");
+                dato.num = 0;
+                fwrite(&dato, sizeof(dato), 1, fich);
+
+                fseek(fich, desplz(-regCont), SEEK_CUR);
+                fread(&dato, sizeof(dato), 1, fich);
+                dato.num = sumatorio;
+                fwrite(&dato, sizeof(dato), 1, fich);
+
+                strcpy(dato.nombre, "Numero:");
+                sumatorio = 0;
+            }
+            else if (cont == 2) {
+                strcpy(dato.nombre, "Multiplicacion:");
+                dato.num = 0;
+                fwrite(&dato, sizeof(dato), 1, fich);
+
+                fseek(fich, desplz(-regCont), SEEK_CUR);
+                fread(&dato, sizeof(dato), 1, fich);
+                dato.num = sumatorio;
+                fwrite(&dato, sizeof(dato), 1, fich);
+
+                strcpy(dato.nombre, "Numero:");
+                sumatorio = 0;
+            }
+        } else {
             dato.num = userNum;
+            sumatorio = sumatorio + dato.num;
+            fwrite(&dato,sizeof (dato),1,fich);
+            regCont++;
         }
-        fwrite(&dato,sizeof (dato),1,fich);
-    } while (cont<3);*/
+    } while (cont<3);
 
     fseek(fich, 0,SEEK_SET);
     fread(&dato,sizeof(dato),1,fich);
--- Programacion/C/Izquierdo Daniel - 10 Funciones predefinidas/main.c
+++ Programacion/C/Izquierdo Daniel - 10 Funciones predefinidas/main.c
@@ -3,10 +3,10 @@
 
 #define TAM 12
 
-short cadInSubcad (char mainString [], char subString [], char auxString []);
+short cadInSubcad (char mainString [], char subString []);
 
 main () {
-    char string [TAM] = "", subString [TAM] = "", auxString [TAM] = "";
+    char string [TAM] = "", subString [TAM] = "";
 
     printf("Introduce una cadena (max %d caracteres): ",TAM-1);
     gets(string);
@@ -14,15 +14,16 @@ main () {
     printf("Introduce una subcadena (max %d caracteres): ",TAM-1);
     gets(subString);
 
-    printf("La subcadena \"%s\"aparece %hd veces en la cadena \"%s\"",subString, cadInSubcad(string,subString,auxString),string);
+    printf("La subcadena \"%s\" aparece %hd veces en la cadena \"%s\" ",subString, cadInSubcad(string,subString),string);
     puts("Fin del proceso");
 
     return 0;
 }
 
-short cadInSubcad (char mainString [], char subString [], char auxString []) {
+short cadInSubcad (char mainString [], char subString []) {
     short contCadena = 0;
     short max = 0;
+    char auxString [TAM] = "";
 
     strcpy(auxString, mainString);
     max=strlen(subString);
