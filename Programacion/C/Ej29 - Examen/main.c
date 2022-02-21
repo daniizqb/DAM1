82e0404c1fcfd9120bffe54c7c04ba409ff54c96
daniizqb <danizquiboni@gmail.com>
2022-02-21 13:28:56 +0100

    21/02/22 13:28

---
+++ Programacion/C/Ej29 - Examen/main.c
@@ -0,0 +1,51 @@
+#include <stdio.h>
+#include <string.h>
+
+#define TAM 11
+
+struct fichContent{
+    char cad1[TAM];
+    char cad2[TAM];
+    char cad3[TAM];
+};
+int main() {
+    struct fichContent f1 = {"","",""};
+
+    FILE *fich;
+    fich=fopen("fichero.bin","wb+");
+
+    char cadena [TAM] = "";
+    short cont = 0;
+
+    strcpy(f1.cad1,"Caracola");
+    strcpy(f1.cad2,"Gominola");
+    strcpy(f1.cad3,"Caracola");
+    fwrite(&f1,sizeof(struct fichContent),1,fich);
+    strcpy(f1.cad1,"Hola");
+    strcpy(f1.cad2,"Fierro");
+    strcpy(f1.cad3,"Caracola");
+    fwrite(&f1,sizeof(struct fichContent),1,fich);
+
+    rewind(fich);
+
+    puts("Introduzca cadena");
+    gets(cadena);
+
+    cont = 0;
+    fread(&f1,sizeof(struct fichContent),1,fich);
+    while (!feof(fich)) {
+        if (!stricmp(f1.cad1,cadena))
+            cont++;
+        if (!stricmp(f1.cad2,cadena))
+            cont++;
+        if (!stricmp(f1.cad3,cadena))
+            cont++;
+        fread(&f1,sizeof(struct fichContent),1,fich);
+    }
+
+    fclose(fich);
+
+    printf("La cadena %s se ha encontrado %hd veces\n",cadena,cont);
+
+    return 0;
+}
