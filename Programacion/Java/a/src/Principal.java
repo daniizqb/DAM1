9161149f7b63c4558e9b377b4404a69a56c2697f
daniizqb <danizquiboni@gmail.com>
2022-03-09 14:18:13 +0100

    09/03/22 14:18

--- Programacion/Java/a/src/Principal.java
+++ Programacion/Java/a/src/Principal.java
@@ -2,10 +2,10 @@ public class Principal {
 
     public static void main(String[] args) {
         String numero = "";
-        final float dec = 375.28f;
+        final float DEC = 375.28f;
         int pos = 0;
         
-        numero = String.valueOf(dec);
+        numero = String.valueOf(DEC);
         pos = numero.lastIndexOf('.');
         System.out.println("Parte entera:" + numero.substring(0,pos) + "\nParte real:" + numero.substring(pos + 1));
     }
