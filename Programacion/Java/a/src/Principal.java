80afd73330d20836b55ac3c5ac9a1e022ed1cb31
daniizqb <danizquiboni@gmail.com>
2022-03-09 14:01:19 +0100

    09/03/22 14:01

--- Programacion/Java/a/src/Principal.java
+++ Programacion/Java/a/src/Principal.java
@@ -1,7 +1,13 @@
 public class Principal {
 
     public static void main(String[] args) {
-
+        String numero = "";
+        final float dec = 375.28f;
+        int pos = 0;
+        
+        numero = String.valueOf(dec);
+        pos = numero.lastIndexOf('.');
+        System.out.println("Parte entera:" + numero.substring(0,pos) + "\nParte real:" + numero.substring(pos + 1));
     }
 
 }
