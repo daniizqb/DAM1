1caeb7bda31a034668aae416baaa8ecd315e41f0
daniizqb <danizquiboni@gmail.com>
2022-03-10 14:16:10 +0100

    10/03/22 14:16

--- Programacion/Java/a/src/Principal.java
+++ Programacion/Java/a/src/Principal.java
@@ -1,13 +1,30 @@
+import java.io.*;
 public class Principal {
 
-    public static void main(String[] args) {
-        String numero = "";
+    public static void main(String[] args) throws IOException {
+        /*String numero = "";
         final float DEC = 375.28f;
         int pos = 0;
         
         numero = String.valueOf(DEC);
         pos = numero.lastIndexOf('.');
-        System.out.println("Parte entera:" + numero.substring(0,pos) + "\nParte real:" + numero.substring(pos + 1));
+        System.out.println("Parte entera:" + numero.substring(0,pos) + "\nParte real:" + numero.substring(pos + 1));*/
+
+        int capital = 0;
+        float interes = 0;
+
+        BufferedReader teclado = new BufferedReader(new InputStreamReader(System.in,"UTF-8"));
+
+        System.out.println("Capital:");
+        capital = Integer.parseInt(teclado.readLine());
+
+        System.out.println("Interes:");
+        interes = Float.parseFloat(teclado.readLine());
+
+        interes = capital/interes;
+        System.out.println("Intereses:" + interes);
+
+        System.out.println("Capital + Interes:" + (capital + interes));
     }
 
 }
