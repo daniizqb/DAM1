c2b1337249322dbcf7230b37db8b640e1a70ac09
daniizqb <danizquiboni@gmail.com>
2022-03-17 09:51:23 +0100

    17/03/22 09:51

--- Programacion/C/2 EV Examen/Ej3/main.c
+++ Programacion/C/2 EV Examen/Ej3/main.c
@@ -10,16 +10,16 @@ void printFich (FILE *fich, struct facturas f1);
 
 int main() {
     struct facturas f1 = {"",0,0};
+    int i = 0;
 
     FILE *fich;
     fich=fopen("facturas.dat","rb+");
 
-    printFich(fich,f1);
-
     fread(&f1,sizeof(struct facturas),1,fich);
     while (!feof(fich)) {
-        if (f1.neto > 0 && f1.neto <= 200) {
+        if (f1.neto > 0 && f1.neto <= 200)
             f1.neto = f1.neto + (f1.neto * 10 / 100);
+<<<<<<< HEAD
             fwrite(&f1.neto, sizeof(struct facturas), 1, fich);
         } else if (f1.neto > 200 && f1.neto <= 500) {
             f1.neto = f1.neto + (f1.neto * 5 / 100);
@@ -28,6 +28,18 @@ int main() {
             f1.neto = f1.neto + (f1.neto * 1 / 100);
             fwrite(&f1.neto, sizeof(struct facturas), 1, fich);
         }
+=======
+        else if (f1.neto > 200 && f1.neto <= 500)
+            f1.neto = f1.neto + (f1.neto * 5 / 100);
+        else if (f1.neto > 500 && f1.neto <= 1000)
+            f1.neto = f1.neto + (f1.neto * 1 / 100);
+            
+        fseek(fich,sizeof (struct facturas) * -1,SEEK_CUR);
+        fwrite(&f1, sizeof(struct facturas), 1, fich);
+        i++;
+        fseek(fich,sizeof (struct facturas) * i,SEEK_SET);
+        
+>>>>>>> 471fcff (14/03/22 14:16)
         fread(&f1,sizeof(struct facturas),1,fich);
     }
     rewind(fich);
