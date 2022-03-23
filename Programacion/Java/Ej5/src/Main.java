dc9ebb5b159b22e6a05755c62b95cf84286fafd3
daniizqb <danizquiboni@gmail.com>
2022-03-23 14:13:04 +0100

    23/03/22 14:13

---
+++ Programacion/Java/Ej5/src/Main.java
@@ -0,0 +1,7 @@
+public class Main {
+    public static void main(String[] args) {
+        Persona pers = new Persona("1111111Q","Juan", (short) 10);
+
+        System.out.println(pers.nif + pers.nombre + pers.edad);
+    }
+}
---
+++ Programacion/Java/Ej5/src/Persona.java
@@ -0,0 +1,10 @@
+public class Persona {
+    String nif;
+    String nombre;
+    short edad;
+    Persona (String n, String nom, short ed) {
+        nif = n;
+        nombre = nom;
+        edad = ed;
+    }
+}
