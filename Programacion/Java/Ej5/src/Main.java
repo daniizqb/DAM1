233a2856fcbfad1bafc3124b268da8802d7be3ad
daniizqb <danizquiboni@gmail.com>
2022-03-24 13:06:00 +0100

    24/03/22 13:05

--- Programacion/Java/Ej5/src/Main.java
+++ Programacion/Java/Ej5/src/Main.java
@@ -1,7 +1,19 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
         Persona pers = new Persona("1111111Q","Juan", (short) 10);
+        Scanner sc = new Scanner(System.in);
 
-        System.out.println(pers.nif + pers.nombre + pers.edad);
+        System.out.printf("NIF: %s Nombre: %s Edad: %hd",pers.getNif(),pers.getNombre(),pers.getEdad());
+
+        System.out.println("NIF");
+        pers.setNif(sc.nextLine());
+        System.out.println("Nombre");
+        pers.setNombre(sc.nextLine());
+        System.out.println("Edad");
+        pers.setEdad(Short.parseShort(sc.nextLine()));
+
+        System.out.printf("NIF: %s Nombre: %s Edad: %hd",pers.getNif(),pers.getNombre(),pers.getEdad());
     }
 }
--- Programacion/Java/Ej5/src/Persona.java
+++ Programacion/Java/Ej5/src/Persona.java
@@ -2,9 +2,34 @@ public class Persona {
     String nif;
     String nombre;
     short edad;
+
     Persona (String n, String nom, short ed) {
         nif = n;
         nombre = nom;
         edad = ed;
+    }
+
+    public String getNif() {
+        return nif;
+    }
+
+    public void setNif(String nif) {
+        this.nif = nif;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public short getEdad() {
+        return edad;
+    }
+
+    public void setEdad(short edad) {
+        this.edad = edad;
     }
 }
