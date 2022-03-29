894b110b6785aa048dbb6b7585844fdccfdc448c
daniizqb <danizquiboni@gmail.com>
2022-03-29 11:37:25 +0200

    29/03/22 11:37

--- Sistemas Informaticos/3 EV/Administración de contraseñas.sh
+++ Sistemas Informaticos/3 EV/Administración de contraseñas.sh
@@ -1,8 +1,32 @@
 #!/bin/bash
+# Ej1
 
-num=("0" "1" "2" "3" "4" "5" "6" "7" "8" "9")
-length=${#num[@]}
+pass='1234'
+length=${#pass}
+echo Numero de caracteres: $length
 
-echo $length
+declare -a nums=('0' '1' '2' '3' '4' '5' '6' '7' '8' '9')
+i=2
 
- for ((i=1;i<=length;i++)) do
+for ((i=0;i<length;i++)) do
+    for ((j=0;j<10;j++)) do
+        if [ ${pass:$i:1} -eq ${nums[$j]} ];then
+            var="$var${nums[$j]}"
+        fi
+    done
+done
+
+echo "$var"
+
+# Ej2
+
+echo Nombre
+read name
+
+echo Pass
+read pass
+
+for ((i=0;i<length;i++)) do
+    ${pass:$i:1}
+    var="$var${nums[$j]}"
+done
