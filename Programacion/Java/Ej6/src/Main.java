82d3664d2fb8767db0b5b256c51e5bd4bda9e127
daniizqb <danizquiboni@gmail.com>
2022-03-30 14:18:22 +0200

    30/03/22 14:18

--- Programacion/Java/Ej6/src/Main.java
+++ Programacion/Java/Ej6/src/Main.java
@@ -4,8 +4,12 @@ import java.util.GregorianCalendar;
 public class Main {
     public static void main(String[] args) {
         /*Pedir Mes y dia y calcular dias que han pasado desde principio de anyo y cuantos maltan hasta final de anyo*/
+        int dia = Integer.parseInt(args[0]);
+        int mes = Integer.parseInt(args[1]) - 1;
 
         GregorianCalendar gc = new GregorianCalendar();
+        gc.set(Calendar.DAY_OF_MONTH,dia);
+        gc.set(Calendar.MONTH,mes);
         System.out.println("Dias transcurridos desde el inico del anyo: " + gc.get(Calendar.DAY_OF_YEAR));
         System.out.println("Dias restantes hasta final del anyo: " + (365 - gc.get(Calendar.DAY_OF_YEAR)));
     }
