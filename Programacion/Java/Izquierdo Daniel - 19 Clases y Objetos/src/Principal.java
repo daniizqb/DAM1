9424c95273bf69bdee361d153c6a97288d8e6863
daniizqb <danizquiboni@gmail.com>
2022-04-06 14:09:58 +0200

    06/04/22 14:09

---
+++ Programacion/Java/Izquierdo Daniel - 19 Clases y Objetos/src/Principal.java
@@ -0,0 +1,41 @@
+import java.util.Scanner;
+
+public class Principal {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        Serie s1 = new Serie();
+        Serie s2 = new Serie("","");
+        Serie s3 = new Serie("",Serie.DEFAULTNUMCAP,Serie.DEFAULTGENRE,"", new String[Serie.MAXACTORS], 0.0f);
+        byte i = 0;
+        String retorno = "";
+
+        System.out.println("||Serie 1||");
+
+        System.out.println("Titulo");
+        s1.setTitulo(sc.nextLine());
+        System.out.println("Guionista");
+        s1.setGuionista(sc.nextLine());
+
+        System.out.println("Actores:");
+        do {
+            System.out.println("Actor:");
+            s1.getActores()[i] = sc.nextLine();
+            System.out.println("Desea continuar introduciendo actores? (S/N)");
+            retorno = sc.nextLine();
+
+            i++;
+        } while (retorno.equalsIgnoreCase("S") && i < Serie.MAXACTORS);
+
+
+        System.out.println("||Serie 2||");
+
+        System.out.println("Titulo");
+        s2.setTitulo(sc.nextLine());
+        System.out.println("Guionista");
+        s2.setGuionista(sc.nextLine());
+
+        System.out.println(s1);
+        System.out.println(s2);
+        System.out.println(s3);
+    }
+}
---
+++ Programacion/Java/Izquierdo Daniel - 19 Clases y Objetos/src/Serie.java
@@ -0,0 +1,100 @@
+import java.util.Arrays;
+
+public class Serie {
+    public static final byte MAXACTORS = 5;
+    public static final char[] genres = {'C','D','S','T'};
+    public static final byte DEFAULTNUMCAP = 10;
+    public static final char DEFAULTGENRE = 'D';
+
+    private String titulo;
+    private byte numCap = DEFAULTNUMCAP;
+    private char genre = DEFAULTGENRE;
+    private String guionista;
+    private String[] actores;
+    private float precio;
+
+    public Serie() {
+        this.actores = new String[MAXACTORS];
+        Arrays.fill(this.actores,"");
+    }
+
+    public Serie(String titulo, String guionista) {
+        this.titulo = titulo;
+        this.guionista = guionista;
+        this.actores = new String[MAXACTORS];
+        Arrays.fill(this.actores,"");
+    }
+
+    public Serie(String titulo, byte numCap, char genre, String guionista, String[] actores, float precio) {
+        this.titulo = titulo;
+        this.numCap = numCap;
+        if (Arrays.binarySearch(genres,genre) >= 0)
+            this.genre = genre;
+        this.guionista = guionista;
+        this.actores = actores;
+        this.precio = precio;
+    }
+
+    public String getTitulo() {
+        return titulo;
+    }
+
+    public void setTitulo(String titulo) {
+        this.titulo = titulo;
+    }
+
+    public byte getNumCap() {
+        return numCap;
+    }
+
+    public void setNumCap(byte numCap) {
+        this.numCap = numCap;
+    }
+
+    public char getgenre() {
+        return genre;
+    }
+
+    public void setgenre(char genre) {
+        if (Arrays.binarySearch(genres,genre) >= 0)
+            this.genre = genre;
+        else
+            this.genre = DEFAULTGENRE;
+    }
+
+    public String getGuionista() {
+        return guionista;
+    }
+
+    public void setGuionista(String guionista) {
+        this.guionista = guionista;
+    }
+
+    public String[] getActores() {
+        return actores;
+    }
+
+    public void setActores(String[] actores) {
+        this.actores = actores;
+    }
+
+    public float getPrecio() {
+        return precio;
+    }
+
+    public void setPrecio(float precio) {
+        this.precio = precio;
+    }
+
+    @Override
+    public String toString() {
+        return "Serie{" +
+                "titulo='" + titulo + '\'' +
+                ", numCap=" + numCap +
+                ", genre=" + genre +
+                ", guionista='" + guionista + '\'' +
+                ", actores=" + Arrays.toString(actores) +
+                ", precio=" + precio +
+                '}';
+    }
+}
