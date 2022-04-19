993a1a0d1717c50fff0d6c63d09e37d4fb774d81
daniizqb <danizquiboni@gmail.com>
2022-04-19 10:16:14 +0200

    Primer Commit

---
+++ Entornos de Desarrollo/3 Trim/UnitTesting2/src/main/java/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+        ArrayNumeros a = new ArrayNumeros();
+
+        a.rellenarArray();
+        a.numDeCadaElemento();
+    }
+}
