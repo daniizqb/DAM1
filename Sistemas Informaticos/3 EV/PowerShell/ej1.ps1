59be4e67f98ee42c21c9384c4265f0dca5d108f7
daniizqb <danizquiboni@gmail.com>
2022-05-09 10:54:24 +0200

    09/05/22 10:54

---
+++ Sistemas Informaticos/3 EV/PowerShell/ej1.ps1
@@ -0,0 +1,3 @@
+$nombre = 'Daniel'
+$saludo = 'KLK manin'
+Write-Host $nombre $saludo
---
+++ Sistemas Informaticos/3 EV/PowerShell/ej4.ps1
@@ -0,0 +1,22 @@
+$n1 = Read-Host 'Num1'
+$n2 = Read-Host 'Num2'
+
+Write-Host 'Multiplicación'
+$aux = $n1 * $n2
+Write-Host $aux
+
+Write-Host 'Division'
+$aux =  $n1 / $n2
+Write-Host $aux
+
+Write-Host 'Suma'
+$aux =  $n1 + $n2
+Write-Host $aux
+
+Write-Host 'Menos'
+$aux =  $n1 - $n2
+Write-Host $aux 
+
+Write-Host 'Resto'
+$aux =  $n1 % $n2
+Write-Host $aux
