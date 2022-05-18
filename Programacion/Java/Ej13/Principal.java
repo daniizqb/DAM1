c5598ad0594c771ba865e6467060c7b5e6ea2c24
daniizqb <danizquiboni@gmail.com>
2022-05-18 14:17:07 +0200

    18/05/22 14:17

--- Programacion/Java/Ej13/Principal.java
+++ Programacion/Java/Ej13/Principal.java
@@ -3,18 +3,6 @@ import java.util.Scanner;
 
 public class Principal {
     public static void main(String[] args) {
-        /*ClaseEnum diaM = ClaseEnum.TUESDAY;
-        ClaseEnum diaJ = ClaseEnum.THURSDAY;
-        System.out.println(diaM.name());
-        System.out.println(diaJ.name());
-
-        System.out.println(diaM.ordinal());
-        ClaseEnum[] dias = ClaseEnum.values();
-
-        for (ClaseEnum d: dias) {
-            System.out.println(d);
-        }*/
-
         System.out.println("El dia obtenido es: " + obtenerDia().toUpperCase());
     }
 
