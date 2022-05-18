3c34251be755ad527bee106e5771beffbe1ab3c2
daniizqb <danizquiboni@gmail.com>
2022-05-18 14:09:03 +0200

    18/05/22 14:09

--- Programacion/Java/Ej13/Principal.java
+++ Programacion/Java/Ej13/Principal.java
@@ -1,6 +1,9 @@
+import java.util.Locale;
+import java.util.Scanner;
+
 public class Principal {
     public static void main(String[] args) {
-        ClaseEnum diaM = ClaseEnum.TUESDAY;
+        /*ClaseEnum diaM = ClaseEnum.TUESDAY;
         ClaseEnum diaJ = ClaseEnum.THURSDAY;
         System.out.println(diaM.name());
         System.out.println(diaJ.name());
@@ -10,6 +13,47 @@ public class Principal {
 
         for (ClaseEnum d: dias) {
             System.out.println(d);
-        }
+        }*/
+
+        System.out.println("El dia obtenido es: " + obtenerDia().toUpperCase());
+    }
+
+    public static String obtenerDia() {
+        ClaseEnum[] dias = ClaseEnum.values();
+
+        Scanner sc = new Scanner(System.in);
+        String dia;
+        boolean f;
+        int i = 0;
+        do {
+            System.out.printf("%s\n",i == 0 ? "Dia de la semana" : "Dia incorrecto");
+            dia = sc.nextLine();
+
+            i = 0;
+            f = false;
+            do {
+                if (dia.toUpperCase().equals(dias[i].name())) {
+                    f = true;
+                }
+                i++;
+            } while (!f && i <= dias.length-1);
+        } while (!f);
+
+        /*do {
+            invalidDay = false;
+            System.out.println("Dia de la semana");
+            dia = sc.nextLine();
+
+            for (ClaseEnum d: dias) {
+                if (d.toString().equalsIgnoreCase(dia)) {
+                    invalidDay = true;
+                }
+            }
+
+            if (!invalidDay)
+                System.out.println("Dia incorrecto");
+        } while (!invalidDay);*/
+
+        return dia;
     }
 }
